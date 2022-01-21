56844574857b8694b01b42efb7b5b5dfa0a0a5ef
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-01-21 19:19:22 -0300

    feat: Navigation programmatic added

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,9 +1,27 @@
+import Link from 'next/link';
 import React from 'react';
 
 const ClientsPage = () => {
+  const clients = [
+    { id: 'max', name: 'Maximiliano' },
+    { id: 'manu', name: 'Manuel' },
+  ];
   return (
     <div>
       <h1>Clients Page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link
+              href={{
+                pathname: '/clients/[id]',
+                query: { id: client.id },
+              }}>
+              {client.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
