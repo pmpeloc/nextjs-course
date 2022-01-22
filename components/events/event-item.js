e7d95be444870019689dc13c4732894ed2c32387
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-01-22 17:57:01 -0300

    feat: Adding Style Module in Event List and Event Item

---
+++ components/events/event-item.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Link from 'next/link';
+
+import classes from './event-item.module.css';
+
+const EventItem = ({ title, image, date, location, id }) => {
+  const humanReadableDate = new Date(date).toLocaleDateString('es-ES', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+
+  const formattedAddress = location.replace(', ', '\n');
+
+  const exploreLink = `/events/${id}`;
+
+  return (
+    <li className={classes.item}>
+      <img src={'/' + image} alt={title} />
+      <div className={classes.content}>
+        <div className={classes.summary}>
+          <h2>{title}</h2>
+          <div className={classes.date}>
+            <time>{humanReadableDate}</time>
+          </div>
+          <div className={classes.address}>
+            <address>{formattedAddress}</address>
+          </div>
+        </div>
+        <div className={classes.actions}>
+          <Link href={exploreLink}>Explore Event</Link>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default EventItem;
