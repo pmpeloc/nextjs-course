05ffbe8ee451c3e09b38fae84dc1d75c26b89d0d
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-01-23 17:29:47 -0300

    feat: Server Side using fs

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import fs from 'fs/promises';
+import path from 'path';
 
 const HomePage = ({ products }) => {
   return (
@@ -11,9 +13,13 @@ const HomePage = ({ products }) => {
 };
 
 export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
   return {
     props: {
-      products: [{ id: 'p1', title: 'Product 1' }],
+      products: data.products,
     },
   };
 }
