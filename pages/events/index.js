468ddf1bd03a9b13ac0bd49b2c2f56f8f7f31ba1
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-01-22 22:29:11 -0300

    feat: Navigation Programmating added

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,14 +1,21 @@
+import { useRouter } from 'next/router';
 import React from 'react';
 import EventList from '../../components/events/event-list';
-import EventsSearch from '../../components/events/event-search';
+import EventsSearch from '../../components/events/events-search';
 import { getAllEvents } from '../../dummy-data';
 
 const AllEventsPage = () => {
   const events = getAllEvents();
+  const router = useRouter();
+
+  const findEventsHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  };
 
   return (
     <div>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </div>
   );
